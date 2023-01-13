9d0ca624aa4c76af10f9325ebb0d2de0f99d06c2
ForsyteRun <forsyterun@gmail.com>
2023-01-13 08:53:16 +0200

    feat: change layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,51 @@
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import Layout from '../components/Layout'
-import { store, wrapper } from '../store'
-import '../styles/globals.scss'
-import { CardDataType } from '../types/types'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import Layout from "../components/Layout";
+import { store, wrapper } from "../store";
+import "../styles/globals.scss";
+import { CardDataType } from "../types/types";
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import React, { FC, ReactNode, useEffect } from "react";
+import { theme } from "../theme/theme";
+import Header from '../components/Header'
 interface Props {
-  items: Array<CardDataType>
+  items: Array<CardDataType>;
 }
 
-const App = ({ Component, pageProps, items}: AppProps & Props)  => {
-
+const App = ({ Component, pageProps, items }: AppProps & Props) => {
   return (
-    <Provider store={store}>
-      <Layout items={items}>
-        <Component {...pageProps}/>
-      </Layout>
-    </Provider>
-  ) 
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container
+        maxWidth={"xl"}
+        sx={{ backgroundColor: theme.palette.background.paper }}
+        className="headerWrapper"
+      >
+        <Header />
+      </Container>
+      <Container
+        maxWidth={"xl"}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          minHeight: "100%",
+        }}
+        className="wrapper"
+      >
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default wrapper.withRedux(App);
-
 
 // App.getInitialProps = async () => {
 //   const res = await fetch('http://localhost:3000/db.json')
 //   const json = await res.json()
 //   return { items: json.pizzas }
 // }
-
 
 //todo: fetch запрос каждый раз при переходе на любую старинц...это плохо
 //todo: margin in container
@@ -36,7 +54,7 @@ export default wrapper.withRedux(App);
 //todo: min 600px how create burger?
 //todo: Tab not in MUI
 //todo: default styles MUI in body
-//todo: del styles which do not need everywhere 
+//todo: del styles which do not need everywhere
 //todo: add palatte => text => default text color
 //todo: типизация компонента App с участием типизации с помощью NextPge
 //todo: NextImage подгружает разніе размеры под оптимизацию  и адаптив... как их привести в исполнение?!
@@ -44,5 +62,3 @@ export default wrapper.withRedux(App);
 //todo: почему при SSR нет фото в network??
 //todo: SortCard костыль исправить
 //todo: можно ли сразу с fetch next.js диспатчить в стор?
-
-
